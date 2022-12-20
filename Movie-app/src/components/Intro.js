a59a499ab877ace72d0304ac91bc44e77042e9b6
minsun0714 <woooooooow22@gmail.com>
2022-12-20 20:37:47 +0900

    🎨 외부 url 버튼 구현 / CSS 적용

--- Movie-app/src/components/Intro.js
+++ Movie-app/src/components/Intro.js
@@ -7,12 +7,13 @@ function Intro({ title, summary, url, genre, rating, bgImg }) {
       <img src={bgImg} alt={title} className={styles.full} />
       <h2 className={styles.white}>{title}</h2>
 
-      <h3 className={styles.white}>{genre.map((g) => `${g}/`)}</h3>
+      <h3 className={styles.genre}>
+        {genre.map((g) => (g !== genre[genre.length - 1] ? `${g}/` : `${g}`))}
+      </h3>
       <h3 className={styles.white}>평점: {rating}</h3>
 
       <button>
-        {" "}
-        <a href={url} target='_blank'>
+        <a href={url} target='_blank' className={styles.btn}>
           Click and watch '{title}' now!
         </a>
       </button>
