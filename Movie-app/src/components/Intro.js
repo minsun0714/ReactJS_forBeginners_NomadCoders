f5c1d582b305cd7c87333d0d63c1f49291ebe2e9
minsun0714 <woooooooow22@gmail.com>
2022-12-21 15:36:21 +0900

    💄 외부 url 버튼 height 높게 변경

--- Movie-app/src/components/Intro.js
+++ Movie-app/src/components/Intro.js
@@ -10,9 +10,9 @@ function Intro({ title, summary, url, genre, rating, bgImg }) {
       <h3 className={styles.genre}>
         {genre.map((g) => (g !== genre[genre.length - 1] ? `${g}/` : `${g}`))}
       </h3>
-      <h3 className={styles.white}>평점: {rating}</h3>
+      <h3 className={styles.rat}>평점: {rating}</h3>
 
-      <button>
+      <button style={{ height: 50 }}>
         <a href={url} target='_blank' className={styles.btn}>
           Click and watch '{title}' now!
         </a>
